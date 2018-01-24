cba62992803541f5843fea5a7df96347419727cb
DeepCode <viacart@gmail.com>
2018-01-24 18:26:47 +0100

    Add Material Theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from './api.service';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { PunteggioComponent } from './punteggio/punteggio.component';
 import { SquadraComponent } from './squadra/squadra.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CustomMaterialModule } from './custom-material.module';
 import { SquadraDetailComponent } from './squadra-detail/squadra-detail.component';
 
 @NgModule({
@@ -26,8 +28,10 @@ import { SquadraDetailComponent } from './squadra-detail/squadra-detail.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CustomMaterialModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [
     ApiService
